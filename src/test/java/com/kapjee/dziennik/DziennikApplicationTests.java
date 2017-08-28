130f6b6b88f62060790ac7ad220daa988221fcc8
fletchPL <wisniewski.maciek1@gmail.com>
2017-08-28 17:29:25 +0200

    initial commit

---
+++ src/test/java/com/kapjee/dziennik/DziennikApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kapjee.dziennik;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DziennikApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
